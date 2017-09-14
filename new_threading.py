e33737821685533327d6d12344d414e1a939ba34
shivanitekriwal1 <shivanitekriwal1@gmail.com>
2017-09-14 23:33:51 +0530

    threading

---
+++ new_threading.py
@@ -0,0 +1,18 @@
+import threading
+
+
+def print_quote():
+    print "This is my thread!"
+
+
+def main():
+
+    our_thread = threading.Thread(target = print_quote)
+    our_thread.start()
+    
+    print threading.active_count()   #prints number of threads running
+    print threading.enumerate()      #lists out the threds running
+    print threading.current_thread() #list out the current thread running
+
+if ( __name__ == "__main__" ):
+    main()
